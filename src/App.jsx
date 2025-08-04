2917f6b84afc097beccaf2a6ff0bfed42f9caacf
Praveen Tetarwal <p.tetarwal808@gmail.com>
2025-08-04 16:46:02 +0530

    implementation of art section

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Navbar from "./Components/Navbar";
 import Hero from "./Components/Hero";
 import Cocktails from "./Components/Cocktails";
 import About from "./Components/About";
+import Art from "./Components/Art";
 
 const App = ()=>{
   return(
@@ -14,6 +15,7 @@ const App = ()=>{
     <Hero/>
     <Cocktails/>
     <About/>
+    <Art/>
    </main>
   );
 }
